55a4719621d63273f0fcd8ba7d0333dcf668ba40
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-12-11 13:46:58 +0100

    add new file

---
+++ Model/GroupInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+* This file is part of the Polymorph.
+*
+* (c) Mlanawo Mbechezi
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Polymorph\Component\User\Model;
+
+/**
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+interface GroupInterface
+{
+    /**
+     * @param string $role
+     *
+     * @return self
+     */
+    public function addRole($role);
+    /**
+     * @return integer
+     */
+    public function getId();
+    /**
+     * @return string
+     */
+    public function getName();
+    /**
+     * @param string $role
+     *
+     * @return boolean
+     */
+    public function hasRole($role);
+    /**
+     * @return array
+     */
+    public function getRoles();
+    /**
+     * @param string $role
+     *
+     * @return self
+     */
+    public function removeRole($role);
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName($name);
+    /**
+     * @param array $roles
+     *
+     * @return self
+     */
+    public function setRoles(array $roles);
+}
